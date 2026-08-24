efaae1cd0744c2d00913fbec0d1a80b63cc203a4
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-24 16:48:27 +0200

    Next one I still need to understand before doing it
    Okay for today enough bye

--- 01_loop/1_while.sh
+++ 01_loop/1_while.sh
@@ -6,3 +6,10 @@ while [ "$INPUT" != "bye" ]; do
 	read INPUT
 	echo "Input was $INPUT"
 done
+
+# now going for always true
+while :; do
+	echo "type in and control c to quit"
+	read INPUT
+	echo "$INPUT"
+done
