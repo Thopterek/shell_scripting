81867dc27b736443f0d93ff7cfae80167996967f
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-31 09:01:59 +0200

    Starting to go over functions and library
    Soon enough I should be done (already 2/3 there) and to start rewriting
    some of the older scripts I have made with less understanding towards
    the actual writing part for them

--- 03_half/1_ext.sh
+++ 03_half/1_ext.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "Time for break from writing shell scripts and going for another part of random thing"
+echo "Comeback! Time to grep!"
+# cut, pulls out pieces of each line of text
+# -d, delimiter split, each line on the : (without on tab)
+# -f(num), where num is a number of field taken
+# rather than using `commnads` just the $(commands)
+echo "user name ${USER}"
+SAVE=$(grep "^${USER}:" /etc/passwd | cut -d: -f1)
+echo $SAVE
+echo "Those two outputs are made in different ways"
+echo "--- added 2>/dev/null to supress errors ---"
+HTML_FILES=$(find / -name "*.html" -print 2>/dev/null)
+echo $HTML_FILES | grep "/index.html$"
+echo "THE WSL ERRORS OF LACK OF ACCESS ARE CRAZY, I might never get to this point"
+find / -name "*.html" -print 2>/dev/null | grep "/index.html$"
