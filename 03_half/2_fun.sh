aa5de9e1709eb56bda7ebafb3ef71916d1c21679
Thopterek <nataniel.dziadzia@gmail.com>
2026-08-31 09:24:02 +0200

    There is no scope of variables
    that's just weird, but boy am I loving it

--- 03_half/2_fun.sh
+++ 03_half/2_fun.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+. ./lib.sh
+
+add_u() {
+	USER=$1
+	PASS=$2
+	shift
+	shift
+	COMM=$@
+	echo "Adding $USER ..."
+	echo useradd -C "$COMM" $USER
+	echo passwd $USER $PASS
+	echo "Adding user $USER ($COMM) with password $PASS"
+}
+
+echo "--- Starting the script ---"
+echo "Script called with $@"
+echo "add some users"
+add_u username_is_dollar_one password_with_\$2 This is part of the comment like Million Dollar the Mr. Krab
+add_u bob password123 Bob Bobbynsky the Jazz Player
+echo "--- Ending adding the users & starting the librarty funcs ---"
+x=1
+echo "x is $x"
+calling_from_lib 1 2 3
+echo "x is $x"
+echo "there is no scope of variables soooooo double checking everything"
--- 03_half/lib.sh
+++ 03_half/lib.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+# procedures:
+# does not return a value but may produce output
+# function
+# returns a single value with no output
+# here both calling fn
+
+calling_from_lib() {
+	echo "I was called as : $@ from library"
+	x=2
+}
